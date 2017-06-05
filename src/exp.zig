5eb66037e948f543be8227912038ec74f91f1aef
Marc Tiehuis <marctiehuis@gmail.com>
2017-06-05 17:00:43 +1200

    Add exp, exp2 and expm1 double functions

--- src/exp.zig
+++ src/exp.zig
@@ -4,21 +4,21 @@ pub fn exp(x: var) -> @typeOf(x) {
     const T = @typeOf(x);
     switch (T) {
         f32 => exp32(x),
-        f64 => unreachable,
+        f64 => exp64(x),
         else => @compileError("exp not implemented for " ++ @typeName(T)),
     }
 }
 
-const half = []const f32 { 0.5, -0.5 };
-const ln2hi = 6.9314575195e-1;
-const ln2lo = 1.4286067653e-6;
-const invln2  = 1.4426950216e+0;
-const P1 = 1.6666625440e-1;
-const P2 = -2.7667332906e-3;
-
 fn exp32(x_: f32) -> f32 {
+    const half = []const f32 { 0.5, -0.5 };
+    const ln2hi = 6.9314575195e-1;
+    const ln2lo = 1.4286067653e-6;
+    const invln2  = 1.4426950216e+0;
+    const P1 = 1.6666625440e-1;
+    const P2 = -2.7667332906e-3;
+
     var x = x_;
-    var hx = fmath.bitCast(u32, x_);
+    var hx = fmath.bitCast(u32, x);
     const sign = hx >> 31;
     hx &= 0x7FFFFFFF;
 
@@ -55,8 +55,9 @@ fn exp32(x_: f32) -> f32 {
             k = i32(1 - sign - sign);
         }
 
-        hi = x - f32(k) * ln2hi;
-        lo = f32(k) * ln2lo;
+        const fk = f32(k);
+        hi = x - fk * ln2hi;
+        lo = fk * ln2lo;
         x = hi - lo;
     }
     // |x| > 2^(-14)
@@ -81,8 +82,89 @@ fn exp32(x_: f32) -> f32 {
     }
 }
 
+fn exp64(x_: f64) -> f64 {
+    const half = []const f64 { 0.5, -0.5 };
+    const ln2hi: f64 = 6.93147180369123816490e-01;
+    const ln2lo: f64 = 1.90821492927058770002e-10;
+    const invln2: f64 = 1.44269504088896338700e+00;
+    const P1: f64   =  1.66666666666666019037e-01;
+    const P2: f64   = -2.77777777770155933842e-03;
+    const P3: f64   =  6.61375632143793436117e-05;
+    const P4: f64   = -1.65339022054652515390e-06;
+    const P5: f64   =  4.13813679705723846039e-08;
+
+    var x = x_;
+    var ux = fmath.bitCast(u64, x);
+    var hx = ux >> 32;
+    const sign = hx >> 31;
+    hx &= 0x7FFFFFFF;
+
+    // |x| >= 708.39 or nan
+    if (hx >= 0x4086232B) {
+        // nan
+        if (hx > 0x7FF00000) {
+            return x;
+        }
+        // TODO: Compare against exact u64 value.
+        if (x > 709.782712893383973096) {
+            // overflow if x != inf
+            return x; // TODO: * 0x1.0p1023;
+        }
+        if (x < -708.39641853226410622) {
+            // underflow if x != -inf
+            // fmath.forceEval(f32(-0x1.0p-149 / x));
+            if (x < -745.13321910194110842) {
+                return 0;
+            }
+        }
+    }
+
+    // argument reduction
+    var k: i32 = undefined;
+    var hi: f64 = undefined;
+    var lo: f64 = undefined;
+
+    // |x| > 0.5 * ln2
+    if (hx > 0x3EB17218) {
+        // |x| >= 1.5 * ln2
+        if (hx > 0x3FF0A2B2) {
+            k = i32(invln2 * x + half[sign]);
+        }
+        else {
+            k = i32(1 - sign - sign);
+        }
+
+        const dk = f64(k);
+        hi = x - dk * ln2hi;
+        lo = dk * ln2lo;
+        x = hi - lo;
+    }
+    // |x| > 2^(-28)
+    else if (hx > 0x3E300000) {
+        k = 0;
+        hi = x;
+        lo = 0;
+    }
+    else {
+        // inexact if x != 0
+        // fmath.forceEval(0x1.0p1023 + x);
+        return 1 + x;
+    }
+
+    const xx = x * x;
+    const c = x - xx * (P1 + xx * (P2 + xx * (P3 + xx * (P4 + xx * P5))));
+    const y = 1 + (x * c / (2 - c) - lo + hi);
+
+    if (k == 0) {
+        y
+    } else {
+        fmath.scalbn(y, k)
+    }
+}
+
 test "exp" {
     fmath.assert(exp(f32(0.0)) == exp32(0.0));
+    fmath.assert(exp(f64(0.0)) == exp64(0.0));
 }
 
 test "exp32" {
@@ -93,4 +175,14 @@ test "exp32" {
     fmath.assert(fmath.approxEq(f32, exp32(0.2), 1.221403, epsilon));
     fmath.assert(fmath.approxEq(f32, exp32(0.8923), 2.440737, epsilon));
     fmath.assert(fmath.approxEq(f32, exp32(1.5), 4.481689, epsilon));
+}
+
+test "exp64" {
+    const epsilon = 0.000001;
+
+    fmath.assert(exp64(0.0) == 1.0);
+    fmath.assert(fmath.approxEq(f64, exp64(0.0), 1.0, epsilon));
+    fmath.assert(fmath.approxEq(f64, exp64(0.2), 1.221403, epsilon));
+    fmath.assert(fmath.approxEq(f64, exp64(0.8923), 2.440737, epsilon));
+    fmath.assert(fmath.approxEq(f64, exp64(1.5), 4.481689, epsilon));
 }
--- src/expm1.zig
+++ src/expm1.zig
@@ -4,19 +4,19 @@ pub fn expm1(x: var) -> @typeOf(x) {
     const T = @typeOf(x);
     switch (T) {
         f32 => expm1f(x),
-        f64 => unreachable,
+        f64 => expm1d(x),
         else => @compileError("exp1m not implemented for " ++ @typeName(T)),
     }
 }
 
-const o_threshold: f32 = 8.8721679688e+01;
-const ln2_hi: f32      = 6.9313812256e-01;
-const ln2_lo: f32      = 9.0580006145e-06;
-const invln2: f32      = 1.4426950216e+00;
-const Q1: f32 = -3.3333212137e-2;
-const Q2: f32 =  1.5807170421e-3;
-
 fn expm1f(x_: f32) -> f32 {
+    const o_threshold: f32 = 8.8721679688e+01;
+    const ln2_hi: f32      = 6.9313812256e-01;
+    const ln2_lo: f32      = 9.0580006145e-06;
+    const invln2: f32      = 1.4426950216e+00;
+    const Q1: f32 = -3.3333212137e-2;
+    const Q2: f32 =  1.5807170421e-3;
+
     var x = x_;
     const ux = fmath.bitCast(u32, x);
     const hx = ux & 0x7FFFFFFF;
@@ -130,8 +130,133 @@ fn expm1f(x_: f32) -> f32 {
     }
 }
 
+fn expm1d(x_: f64) -> f64 {
+    const o_threshold: f64 = 7.09782712893383973096e+02;
+    const ln2_hi: f64      = 6.93147180369123816490e-01;
+    const ln2_lo: f64      = 1.90821492927058770002e-10;
+    const invln2: f64      = 1.44269504088896338700e+00;
+    const Q1: f64 = -3.33333333333331316428e-02;
+    const Q2: f64 =  1.58730158725481460165e-03;
+    const Q3: f64 = -7.93650757867487942473e-05;
+    const Q4: f64 =  4.00821782732936239552e-06;
+    const Q5: f64 = -2.01099218183624371326e-07;
+
+    var x = x_;
+    const ux = fmath.bitCast(u64, x);
+    const hx = u32(ux >> 32) & 0x7FFFFFFF;
+    const sign = hx >> 63;
+
+    // |x| >= 56 * ln2
+    if (hx >= 0x4043687A) {
+        // exp1md(nan) = nan
+        if (hx > 0x7FF00000) {
+            return x;
+        }
+        // exp1md(-ve) = -1
+        if (sign != 0) {
+            return -1;
+        }
+        if (x > o_threshold) {
+            return x; // TODO: * 0x1.0p1023;
+        }
+    }
+
+    var hi: f64 = undefined;
+    var lo: f64 = undefined;
+    var c: f64 = undefined;
+    var k: i32 = undefined;
+
+    // |x| > 0.5 * ln2
+    if (hx > 0x3FD62E42) {
+        // |x| < 1.5 * ln2
+        if (hx < 0x3FF0A2B2) {
+            if (sign == 0) {
+                hi = x - ln2_hi;
+                lo = ln2_lo;
+                k = 1;
+            } else {
+                hi = x + ln2_hi;
+                lo = -ln2_lo;
+                k = -1;
+            }
+        } else {
+            var kf = invln2 * x;
+            if (sign != 0) {
+                kf -= 0.5;
+            } else {
+                kf += 0.5;
+            }
+
+            k = i32(kf);
+            const t = f64(k);
+            hi = x - t * ln2_hi;
+            lo = t * ln2_lo;
+        }
+
+        x = hi - lo;
+        c = (hi - x) - lo;
+    }
+    // |x| < 2^(-54)
+    else if (hx < 0x3C900000) {
+        if (hx < 0x00100000) {
+            fmath.forceEval(f32(x));
+        }
+        return x;
+    }
+    else {
+        k = 0;
+    }
+
+    const hfx = 0.5 * x;
+    const hxs = x * hfx;
+    const r1 = 1.0 + hxs * (Q1 + hxs * (Q2 + hxs * (Q3 + hxs * (Q4 + hxs * Q5))));
+    const t = 3.0 - r1 * hfx;
+    var e = hxs * ((r1 - t) / (6.0 - x * t));
+
+    // c is 0
+    if (k == 0) {
+        return x - (x * e - hxs);
+    }
+
+    e = x * (e - c) - c;
+    e -= hxs;
+
+    // exp(x) ~ 2^k (x_reduced - e + 1)
+    if (k == -1) {
+        return 0.5 * (x - e) - 0.5;
+    }
+    if (k == 1) {
+        if (x < -0.25) {
+            return -2.0 * (e - (x + 0.5));
+        } else {
+            return 1.0 + 2.0 * (x - e);
+        }
+    }
+
+    const twopk = fmath.bitCast(f64, u64(0x3FF + k) << 52);
+
+    if (k < 0 or k > 56) {
+        var y = x - e + 1.0;
+        if (k == 1024) {
+            y = y * 2.0; // TODO: * 0x1.0p1023;
+        } else {
+            y = y * twopk;
+        }
+
+        return y - 1.0;
+    }
+
+    const uf = fmath.bitCast(f64, u64(0x3FF - k) << 52);
+    if (k < 20) {
+        return (x - e + (1 - uf)) * twopk;
+    } else {
+        return (x - (e + uf) + 1) * twopk;
+    }
+}
+
 test "exp1m" {
     fmath.assert(expm1(f32(0.0)) == expm1f(0.0));
+    fmath.assert(expm1(f64(0.0)) == expm1d(0.0));
 }
 
 test "expm1f" {
@@ -142,4 +267,14 @@ test "expm1f" {
     fmath.assert(fmath.approxEq(f32, expm1f(0.2), 0.221403, epsilon));
     fmath.assert(fmath.approxEq(f32, expm1f(0.8923), 1.440737, epsilon));
     fmath.assert(fmath.approxEq(f32, expm1f(1.5), 3.481689, epsilon));
+}
+
+test "expm1d" {
+    const epsilon = 0.000001;
+
+    fmath.assert(expm1d(0.0) == 0.0);
+    fmath.assert(fmath.approxEq(f64, expm1d(0.0), 0.0, epsilon));
+    fmath.assert(fmath.approxEq(f64, expm1d(0.2), 0.221403, epsilon));
+    fmath.assert(fmath.approxEq(f64, expm1d(0.8923), 1.440737, epsilon));
+    fmath.assert(fmath.approxEq(f64, expm1d(1.5), 3.481689, epsilon));
 }
