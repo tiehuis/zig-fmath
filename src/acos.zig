efb530f9b2551d0add55d50840fb95026f02659d
Marc Tiehuis <marctiehuis@gmail.com>
2017-06-07 18:40:23 +1200

    Add acos, asin, atan and atan2 double functions

--- src/acos.zig
+++ src/acos.zig
@@ -4,25 +4,26 @@ pub fn acos(x: var) -> @typeOf(x) {
     const T = @typeOf(x);
     switch (T) {
         f32 => acos32(x),
-        f64 => unreachable,
+        f64 => acos64(x),
         else => @compileError("acos not implemented for " ++ @typeName(T)),
     }
 }
 
-const pio2_hi = 1.5707962513e+00; // 0x3FC90FDA
-const pio2_lo = 7.5497894159e-08; // 0x33A22168
-const pS0 =  1.6666586697e-01;
-const pS1 = -4.2743422091e-02;
-const pS2 = -8.6563630030e-03;
-const qS1 = -7.0662963390e-01;
-
 fn r32(z: f32) -> f32 {
+    const pS0 =  1.6666586697e-01;
+    const pS1 = -4.2743422091e-02;
+    const pS2 = -8.6563630030e-03;
+    const qS1 = -7.0662963390e-01;
+
     const p = z * (pS0 + z * (pS1 + z * pS2));
     const q = 1.0 + z * qS1;
     p / q
 }
 
 fn acos32(x: f32) -> f32 {
+    const pio2_hi = 1.5707962513e+00;
+    const pio2_lo = 7.5497894159e-08;
+
     const hx: u32 = fmath.bitCast(u32, x);
     const ix: u32 = hx & 0x7FFFFFFF;
 
@@ -57,7 +58,7 @@ fn acos32(x: f32) -> f32 {
     }
 
     // x > 0.5
-    const z = (1 - x) * 0.5;
+    const z = (1.0 - x) * 0.5;
     const s = fmath.sqrt(z);
     const jx = fmath.bitCast(u32, s);
     const df = fmath.bitCast(f32, jx & 0xFFFFF000);
@@ -66,8 +67,78 @@ fn acos32(x: f32) -> f32 {
     2 * (df + w)
 }
 
+fn r64(z: f64) -> f64 {
+    const pS0: f64 =  1.66666666666666657415e-01;
+    const pS1: f64 = -3.25565818622400915405e-01;
+    const pS2: f64 =  2.01212532134862925881e-01;
+    const pS3: f64 = -4.00555345006794114027e-02;
+    const pS4: f64 =  7.91534994289814532176e-04;
+    const pS5: f64 =  3.47933107596021167570e-05;
+    const qS1: f64 = -2.40339491173441421878e+00;
+    const qS2: f64 =  2.02094576023350569471e+00;
+    const qS3: f64 = -6.88283971605453293030e-01;
+    const qS4: f64 =  7.70381505559019352791e-02;
+
+    const p = z * (pS0 + z * (pS1 + z * (pS2 + z * (pS3 + z * (pS4 + z * pS5)))));
+    const q = 1.0 + z * (qS1 + z * (qS2 + z * (qS3 + z * qS4)));
+    p / q
+}
+
+fn acos64(x: f64) -> f64 {
+    const pio2_hi: f64 = 1.57079632679489655800e+00;
+    const pio2_lo: f64 = 6.12323399573676603587e-17;
+
+    const ux = fmath.bitCast(u64, x);
+    const hx = u32(ux >> 32);
+    const ix = hx & 0x7FFFFFFF;
+
+    // |x| >= 1 or nan
+    if (ix >= 0x3FF00000) {
+        const lx = u32(ux & 0xFFFFFFFF);
+
+        // acos(1) = 0, acos(-1) = pi
+        if ((ix - 0x3FF00000) | lx == 0) {
+            if (hx >> 31 != 0) {
+                return 2 * pio2_hi + 0x1.0p-120;
+            } else {
+                return 0;
+            }
+        }
+
+        return 0 / (x - x);
+    }
+
+    // |x| < 0.5
+    if (ix < 0x3FE00000) {
+        // |x| < 2^(-57)
+        if (ix <= 0x3C600000) {
+            return pio2_hi + 0x1.0p-120;
+        } else {
+            return pio2_hi - (x - (pio2_lo - x * r64(x * x)));
+        }
+    }
+
+    // x < -0.5
+    if (hx >> 31 != 0) {
+        const z = (1.0 + x) * 0.5;
+        const s = fmath.sqrt(z);
+        const w = r64(z) * s - pio2_lo;
+        return 2 * (pio2_hi - (s + w));
+    }
+
+    // x > 0.5
+    const z = (1.0 - x) * 0.5;
+    const s = fmath.sqrt(z);
+    const jx = fmath.bitCast(u64, s);
+    const df = fmath.bitCast(f64, jx & 0xFFFFFFFF00000000);
+    const c = (z - df * df) / (s + df);
+    const w = r64(z) * s + c;
+    2 * (df + w)
+}
+
 test "acos" {
     fmath.assert(acos(f32(0.0)) == acos32(0.0));
+    fmath.assert(acos(f64(0.0)) == acos64(0.0));
 }
 
 test "acos32" {
@@ -79,4 +150,15 @@ test "acos32" {
     fmath.assert(fmath.approxEq(f32, acos32(0.5), 1.047198, epsilon));
     fmath.assert(fmath.approxEq(f32, acos32(0.8923), 0.468382, epsilon));
     fmath.assert(fmath.approxEq(f32, acos32(-0.2), 1.772154, epsilon));
+}
+
+test "acos64" {
+    const epsilon = 0.000001;
+
+    fmath.assert(fmath.approxEq(f64, acos64(0.0), 1.570796, epsilon));
+    fmath.assert(fmath.approxEq(f64, acos64(0.2), 1.369438, epsilon));
+    fmath.assert(fmath.approxEq(f64, acos64(0.3434), 1.220262, epsilon));
+    fmath.assert(fmath.approxEq(f64, acos64(0.5), 1.047198, epsilon));
+    fmath.assert(fmath.approxEq(f64, acos64(0.8923), 0.468382, epsilon));
+    fmath.assert(fmath.approxEq(f64, acos64(-0.2), 1.772154, epsilon));
 }
--- src/atan2.zig
+++ src/atan2.zig
@@ -3,7 +3,7 @@ const fmath = @import("index.zig");
 pub fn atan2(comptime T: type, x: T, y: T) -> T {
     switch (T) {
         f32 => atan2f(x, y),
-        f64 => unreachable,
+        f64 => atan2d(x, y),
         else => @compileError("atan2 not implemented for " ++ @typeName(T)),
     }
 }
@@ -93,8 +93,99 @@ fn atan2f(y: f32, x: f32) -> f32 {
     }
 }
 
+fn atan2d(y: f64, x: f64) -> f64 {
+    const pi: f64    = 3.1415926535897931160E+00;
+    const pi_lo: f64 = 1.2246467991473531772E-16;
+
+    if (fmath.isNan(x) or fmath.isNan(y)) {
+        return x + y;
+    }
+
+    var ux = fmath.bitCast(u64, x);
+    var ix = u32(ux >> 32);
+    var lx = u32(ux & 0xFFFFFFFF);
+
+    var uy = fmath.bitCast(u64, y);
+    var iy = u32(uy >> 32);
+    var ly = u32(uy & 0xFFFFFFFF);
+
+    // x = 1.0
+    if ((ix -% 0x3FF00000) | lx == 0) {
+        return fmath.atan(y);
+    }
+
+    // 2 * sign(x) + sign(y)
+    const m = ((iy >> 31) & 1) | ((ix >> 30) & 2);
+    ix &= 0x7FFFFFFF;
+    iy &= 0x7FFFFFFF;
+
+    if (iy | ly == 0) {
+        switch (m) {
+            0, 1 => return  y,          // atan(+-0, +...)
+            2    => return  pi,         // atan(+0, -...)
+            3    => return -pi,         // atan(-0, -...)
+            else => unreachable,
+        }
+    }
+
+    if (ix | lx == 0) {
+        if (m & 1 != 0) {
+            return -pi / 2;
+        } else {
+            return pi / 2;
+        }
+    }
+
+    if (ix == 0x7FF00000) {
+        if (iy == 0x7FF00000) {
+            switch (m) {
+                0    => return    pi / 4,   // atan(+inf, +inf)
+                1    => return   -pi / 4,   // atan(-inf, +inf)
+                2    => return  3*pi / 4,   // atan(+inf, -inf)
+                3    => return -3*pi / 4,   // atan(-inf, -inf)
+                else => unreachable,
+            }
+        } else {
+            switch (m) {
+                0    => return  0.0,   // atan(+..., +inf)
+                1    => return -0.0,   // atan(-..., +inf)
+                2    => return   pi,   // atan(+..., -inf)
+                3    => return  -pi,   // atan(-...f, -inf)
+                else => unreachable,
+            }
+        }
+    }
+
+    // |y / x| > 0x1p64
+    if (ix +% (64 << 20) < iy or iy == 0x7FF00000) {
+        if (m & 1 != 0) {
+            return -pi / 2;
+        } else {
+            return pi / 2;
+        }
+    }
+
+    // z = atan(|y / x|) with correct underflow
+    var z = {
+        if ((m & 2) != 0 and iy +% (64 << 20) < ix) {
+            0.0
+        } else {
+            fmath.atan(fmath.fabs(y / x))
+        }
+    };
+
+    switch (m) {
+        0    => return  z,                  // atan(+, +)
+        1    => return -z,                  // atan(-, +)
+        2    => return pi - (z - pi_lo),    // atan(+, -)
+        3    => return (z - pi_lo) - pi,    // atan(-, -)
+        else => unreachable,
+    }
+}
+
 test "atan2" {
     fmath.assert(atan2(f32, 0.2, 0.21) == atan2f(0.2, 0.21));
+    fmath.assert(atan2(f64, 0.2, 0.21) == atan2d(0.2, 0.21));
 }
 
 test "atan2f" {
@@ -107,4 +198,16 @@ test "atan2f" {
     fmath.assert(fmath.approxEq(f32, atan2f(-0.2, -0.2), -2.356194, epsilon));
     fmath.assert(fmath.approxEq(f32, atan2f(0.34, -0.4), 2.437099, epsilon));
     fmath.assert(fmath.approxEq(f32, atan2f(0.34, 1.243), 0.267001, epsilon));
+}
+
+test "atan2d" {
+    const epsilon = 0.000001;
+
+    fmath.assert(fmath.approxEq(f64, atan2d(0.0, 0.0), 0.0, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(0.2, 0.2), 0.785398, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(-0.2, 0.2), -0.785398, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(0.2, -0.2), 2.356194, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(-0.2, -0.2), -2.356194, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(0.34, -0.4), 2.437099, epsilon));
+    fmath.assert(fmath.approxEq(f64, atan2d(0.34, 1.243), 0.267001, epsilon));
 }
